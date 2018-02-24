d2d5f3ab8d2869e4db897939cf0c4e780738359c
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-02-24 09:37:32 -0800

    Add is mobile view helper to service config

--- src/Module.php
+++ src/Module.php
@@ -2,6 +2,7 @@
 namespace LeoGalleguillos\Superglobal;
 
 use LeoGalleguillos\Superglobal\Model\Service as SuperglobalService;
+use LeoGalleguillos\Superglobal\View\Helper as SuperglobalHelper;
 
 class Module
 {
@@ -10,8 +11,16 @@ class Module
         return [
             'view_helpers' => [
                 'aliases' => [
+                    'isMobile' => SuperglobalHelper\Server\HttpUserAgent\IsMobile::class,
                 ],
                 'factories' => [
+                    SuperglobalHelper\Server\HttpUserAgent\IsMobile::class => function ($serviceManager) {
+                        return new SuperglobalHelper\Server\HttpUserAgent\IsMobile(
+                            $serviceManager->get(
+                                SuperglobalService\Server\HttpUserAgent\IsMobile::class
+                            )
+                        );
+                    },
                 ],
             ],
         ];
