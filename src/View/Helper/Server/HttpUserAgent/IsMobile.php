18114cead2d6aae5f55c0696d3706871859a8ff9
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-02-26 02:38:56 -0800

    Code cleanup

--- src/View/Helper/Server/HttpUserAgent/IsMobile.php
+++ src/View/Helper/Server/HttpUserAgent/IsMobile.php
@@ -2,11 +2,15 @@
 namespace LeoGalleguillos\Superglobal\View\Helper\Server\HttpUserAgent;
 
 use LeoGalleguillos\Superglobal\Model\Service as SuperglobalService;
-
 use Zend\View\Helper\AbstractHelper;
 
 class IsMobile extends AbstractHelper
 {
+    /**
+     * Construct
+     *
+     * @param SuperglobalService\Server\HttpUserAgent\IsMobile $isMobileService
+     */
     public function __construct(
         SuperglobalService\Server\HttpUserAgent\IsMobile $isMobileService
     ) {
